2db87a2daa1958436e36350e9e9e406d5184d560
thallesarouca <trssarouca@hotmail.com>
2022-11-07 18:59:19 -0300

    Criando um sistema e abstraindo um celular com POO e C#

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,22 +3,26 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string Imei { get; set; }
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
-            Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Numero = numero;   
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine($"Ligando... para { Numero}");
         }
 
         public void ReceberLigacao()
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine($"Recebendo ligação... de {Numero} ");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("\nSmartphone Nokia:");
+var nokia = new Nokia("(99) 99999-9999", "Modelo 2.40", "123456789", 256);
+nokia.Ligar();
+nokia.InstalarAplicativo(" - Programando em C# -");
+
+Console.WriteLine("\nSmartphone Iphone:");
+var iphone = new Iphone("(88) 88888-8888", "versao 2.42 ", "123456789", 64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("- Programando em C# - ");
